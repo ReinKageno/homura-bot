b530f1af56342491185241b49f1905e3661a77c5
Kanade <hirotakanarein@gmail.com>
2026-07-24 21:55:05 +0700

    Add 'slap' command and improve descriptions for 'cry', 'hug', 'pat', and 'look' commands
    v.3.3

--- commands/fun.py
+++ commands/fun.py
@@ -7,21 +7,25 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.hybrid_command(name='cry', description="Just cry")
+    async def cry(self, ctx:commands.Context):
+        await gifs.send_gif(ctx, None, 'cry')
+
     @commands.hybrid_command(name='look', description="Look at my-")
     async def look(self, ctx:commands.Context):
         await gifs.send_gif(ctx, None, 'look')
-
-    @commands.hybrid_command(name='pat', description="Get a pat or give to someone")
-    async def pat(self, ctx:commands.Context, member:discord.Member=None):
-        await gifs.send_gif(ctx, member, 'pat')
 
     @commands.hybrid_command(name='hug', description="A hug can make you feel warm")
     async def hug(self, ctx:commands.Context, member:discord.Member=None):
         await gifs.send_gif(ctx, member, 'hug')
 
-    @commands.hybrid_command(name='cry', description="Just cry")
-    async def cry(self, ctx:commands.Context):
-        await gifs.send_gif(ctx, None, 'cry')
+    @commands.hybrid_command(name='pat', description="Get a pat or give to someone")
+    async def pat(self, ctx:commands.Context, member:discord.Member=None):
+        await gifs.send_gif(ctx, member, 'pat')
+
+    @commands.hybrid_command(name='slap', description="Feel annoyed?")
+    async def slap(self, ctx:commands.Context, member:discord.Member=None):
+        await gifs.send_gif(ctx, member, 'slap')
 
 async def setup(bot):
     await bot.add_cog(Fun(bot))
